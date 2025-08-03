90a2b5733da27b24e3922514c30f5b34c04c2912
kamoo28 <cmallah96@gmail.com>
2025-08-03 15:36:31 +0530

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,11 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+// {"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      footer
+    </div>
+  );
+};
+
+export default Footer;
